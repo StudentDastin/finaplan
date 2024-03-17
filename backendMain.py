11bb608226fcf2aae9cd46cc686d6e1d847627fe
Dastin <dastin.rennspiess@dci-student.org>
2024-03-17 22:29:01 +0100

    played around in the backend file. no real content yet.

---
+++ backendMain.py
@@ -0,0 +1,17 @@
+class controller:
+
+    def iniziator(self, a):
+        test=0
+        print("print bin der Methode")
+        return "ende der methode"
+        
+    def main(self):
+        print("bin in der Main")    
+        instanz.iniziator("hund")
+        print("ende der main")
+    
+
+instanz = controller()
+instanz.main()
+
+#I'm still finding out. I don't think that's correct. The content is purely experimental to test.
